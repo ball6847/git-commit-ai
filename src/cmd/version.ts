dd7b388c257e7a7c0c03ec42f8f213839dae3c67
Porawit Poboonma <ball6847@gmail.com>
2026-04-15 00:54:46 +0700

    chore: bump version to 1.0.3

--- src/cmd/version.ts
+++ src/cmd/version.ts
@@ -1,12 +1,8 @@
 import { bold, cyan } from '@std/fmt/colors';
 import type { Logger } from '../services.ts';
-import { join } from '@std/path';
+import denoJson from '../../deno.json' with { type: 'json' };
 
-const DENO_JSON_PATH = join(import.meta.dirname!, '..', '..', 'deno.json');
-
-export const VERSION: string = JSON.parse(
-  await Deno.readTextFile(DENO_JSON_PATH),
-).version;
+export const VERSION: string = denoJson.version;
 
 export interface VersionDependencies {
   logger?: Logger;
